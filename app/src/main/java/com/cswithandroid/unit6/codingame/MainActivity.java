1408575df00076b2cd6af17b1cffdf0a26b91f21
sahiljain112 <sahiljain112@gmail.com>
2016-10-01 02:01:43 +0530

    layout enhancement

--- app/src/main/java/com/cswithandroid/unit6/codingame/MainActivity.java
+++ app/src/main/java/com/cswithandroid/unit6/codingame/MainActivity.java
@@ -2,12 +2,25 @@ package com.cswithandroid.unit6.codingame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bEnter, bDeclare;
+    private TextView tvPlayer1,tvPlayer2, tvWord, tvWordList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bEnter = (Button) findViewById( R.id.bEnter);
+        bDeclare = (Button) findViewById( R.id.bDeclare);
+
+        tvPlayer1 = (TextView) findViewById (R.id.tvPlayer1);
+        tvPlayer2 = (TextView) findViewById (R.id.tvPlayer2);
+        tvWord = (TextView) findViewById( R.id.Word);
+        tvWordList = (TextView) findViewById( R.id.tvWordList);
     }
 }
