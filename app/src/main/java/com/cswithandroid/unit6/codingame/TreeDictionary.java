97cde11e5983453f97ea113421ef722791e2f353
sahiljain112 <sahiljain112@gmail.com>
2016-10-03 19:52:02 +0530

    Tested with actual dictionary

--- app/src/main/java/com/cswithandroid/unit6/codingame/TreeDictionary.java
+++ app/src/main/java/com/cswithandroid/unit6/codingame/TreeDictionary.java
@@ -6,10 +6,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.TreeSet;
 
 /**
@@ -85,7 +83,7 @@ public class TreeDictionary {
           if(firstChar != lastChar)
             return false;
         }
-         return !wordSet.contains(word); // && t.contains(word);
+         return !wordSet.contains(word) && t.contains(word);
     }
 
     public boolean addNewWord(String word){
